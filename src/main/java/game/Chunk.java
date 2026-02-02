17d4959312ea8f62307e681f3740c2488fb85cc8
matthewprague69 <matous.prazakg@gmail.com>
2026-02-02 03:39:50 +0100

    Clamp impostor textures to avoid halos

--- src/main/java/game/Chunk.java
+++ src/main/java/game/Chunk.java
@@ -1008,6 +1008,8 @@ public class Chunk {
                 0, GL_RGBA, GL_UNSIGNED_BYTE, (java.nio.ByteBuffer) null);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
         int fbo = glGenFramebuffers();
         glBindFramebuffer(GL_FRAMEBUFFER, fbo);
