3b4ce391bf34ed05e4a2939a2b8d103b98cdcb56
matthewprague69 <matous.prazakg@gmail.com>
2026-02-02 04:07:11 +0100

    Use hard alpha for tree leaves

--- src/main/java/objects/Tree.java
+++ src/main/java/objects/Tree.java
@@ -467,8 +467,7 @@ public class Tree extends Feature {
         }
 
         glEnable(GL_TEXTURE_2D);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glDisable(GL_BLEND);
         glDepthMask(true);
         glEnable(GL_ALPHA_TEST);
         glAlphaFunc(GL_GREATER, 0.5f);
@@ -495,8 +494,7 @@ public class Tree extends Feature {
         }
 
         glEnable(GL_TEXTURE_2D);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glDisable(GL_BLEND);
         glDepthMask(true);
         glEnable(GL_ALPHA_TEST);
         glAlphaFunc(GL_GREATER, 0.5f);
